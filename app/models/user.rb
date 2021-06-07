f11cf90e80638f7411ef7528814b85e27b16f792
jocelin queau <queau.j@gmail.com>
2021-06-07 19:39:07 +0200

    resolving conflit in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
 
   has_one :cart, dependent: :destroy
   after_create :user_cart
@@ -11,13 +10,10 @@ class User < ApplicationRecord
   def user_cart
     Cart.create(user_id: self.id)
   end
-||||||| 16ee47c
-=======
-  after_create :welcome_email
+
+  #after_create :welcome_email
 
   def welcome_email
     UserMailer.welcome_email(self).deliver
   end
-
->>>>>>> development
 end
