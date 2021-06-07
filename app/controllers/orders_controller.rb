a3b827d1d0afadff4d37c5cb2c88ce04d6db8370
Xabi <xabi.aycaguer@gmail.com>
2021-06-07 16:41:16 +0200

    add order controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,27 @@
+class OrdersController < ApplicationController
+  before_action :authenticate?
+
+  
+  def show 
+    @order = Order.find(params[:id])
+  end
+
+  def create 
+    @user = current_user
+    @order = Order.new(user_id:@user.id)
+    @user.cart.items.each do |item|
+      JointItemsOrder.create(order_id:@order.id,item_id:item.id)
+    end
+    if @order.save 
+      redirect_to root_path, notice: "La commande est bien passé"
+    else
+      flash.now[:danger] = @order.errors.full_messages
+      render new_event_path
+    end
+
+  end
+  
+  private
+
+
+end
