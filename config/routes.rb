701d3d56e43d8e5e01a9dae9da237a52828a18a6
jocelin queau <queau.j@gmail.com>
2021-06-07 19:37:57 +0200

    minor error in route due to conflit ||||

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root "static_pages#index"
-  #get "static_pages/index"
-
-  resources :cart, only: [:update, :destroy]
-
-||||||| 16ee47c
-  get 'user/show'
-  get 'item/show'
-  root "static_pages#index"
-  #get "static_pages/index"
-=======
->>>>>>> development
   devise_for :users
-  
+
   resources :users, only: [:show] do
-    resources :carts, only: [:show,:edit,:update] do
+    resources :carts, only: [:show, :edit, :update] do
       resources :orders, only: [:show]
     end
   end
 
-  resources :items, only: [:show]  
+  resources :items, only: [:show]
   root "static_pages#index"
-
 
   resources :users, only: [:show] do
     resources :carts, only: [:show, :edit, :update]
