f87942546d5c8a2fc24d34c236d9519b4ed4e16a
Xabi <xabi.aycaguer@gmail.com>
2021-06-07 16:33:32 +0200

    add order controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_devise_parameters, if: :devise_controller?
+
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :description, :email, :password, :password_confirmation, :admin)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :description, :email, :password, :password_confirmation, :current_password, :admin)}
+  end
+
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
+  end
+
+  def login?
+    !!current_user()
+  end
+
+  def authenticated?
+    redirect_to new_user_session_path unless login?()
+  end
+    
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   validates :user, 
     presence: true
 
-  has_many   :joint_items_orders
+  has_many   :joint_items_orders, dependent: :destroy
   has_many   :items, through: :joint_items_orders
   belongs_to :user
   
