cf1810df0ce27d1e7a1e9c1a371c57675afc69c5
Xabi <xabi.aycaguer@gmail.com>
2021-06-11 01:42:11 +0200

    mailer image

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,6 @@
 module EmailHelper
     def email_image_tag(image, **options)
-      attachments[image] = File.read(Rails.root.join("rails/active_storage/disk/#{image}"))
+      attachments[image] = File.read(Rails.root.join("rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBFZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--#{image}"))
       image_tag attachments[image].url, **options
     end
 end
