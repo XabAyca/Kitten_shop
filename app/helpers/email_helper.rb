d3d9b1f9b9083bc302244f8575f6a85177bc7a08
Lehoax <leolair.pro@gmail.com>
2021-06-10 16:34:55 -0700

    add path

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,6 @@
 module EmailHelper
     def email_image_tag(image, **options)
-      attachments[image] = File.read(Rails.root.join("rails/active_storage/blobs/redirect/#{image}"))
+      attachments[image] = File.read(Rails.root.join("rails/active_storage/disk/#{image}"))
       image_tag attachments[image].url, **options
     end
 end
