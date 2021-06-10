0c4a4f2b77acdc7860383e9a0123c1fd5767af92
Lehoax <leolair.pro@gmail.com>
2021-06-10 16:16:32 -0700

    add url

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,6 @@
 module EmailHelper
     def email_image_tag(image, **options)
-      attachments[image] = File.read(Rails.root.join("app/assets/images/#{image}"))
+      attachments[image] = File.read(Rails.root.join("rails/active_storage/blobs/redirect/#{image}"))
       image_tag attachments[image].url, **options
     end
 end
