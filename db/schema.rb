72f9ee20a464b5aff2c93ecba01dc3d430b6d9a7
Xabi <xabi.aycaguer@gmail.com>
2021-06-10 11:50:56 +0200

    css form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_082557) do
+ActiveRecord::Schema.define(version: 2021_06_10_090057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2021_06_09_082557) do
     t.string "title"
     t.text "description"
     t.decimal "price"
-    t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
